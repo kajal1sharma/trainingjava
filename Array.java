79627546f75fc274ad4652009fdd73729578d284
kajal <kajal@gmail.com>
2023-12-28 13:59:25 +0530

    ..

---
+++ Array.java
@@ -0,0 +1,57 @@
+public class Array {
+    public static void main(String[] args) {
+        
+        // array is collection of similar typ eof data
+
+        //datatype nameof array = size
+
+        // int arr[] = new int[5];
+
+        int student1=90;
+        int student2 =80;
+        int student3 =70;
+
+        int marks[] = new int[5];//0 
+        marks[0] =40;
+        marks[1]=33;
+        marks[2] = 50;
+        marks[3]=74;
+        marks[4] = 60;
+
+        // Q:minimum value inside the array
+        // Q:maximum number inside the array
+        // Q:sort the array
+
+        // int sum = marks[0]+marks[1]+marks[2]+marks[3]+marks[4];
+        // int sum=0;
+        // for(int i=0;i<marks.length ; i++){
+        //     sum =sum +marks[i];
+        // }
+        // System.out.println(sum);
+
+        // int sum =1;
+
+        // for(int i = 0 ; i<marks.length;i++){
+        //     if(marks[i]%5 ==0 ){
+        //         sum =sum*marks[i];
+        //     }
+
+        // }
+        // System.out.println(sum);
+
+        // DRY =>dont repeat yourself
+
+        // for(int i =0;i < marks.length;i++){
+        //     System.out.println(marks[i]);
+        // }
+
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        // System.out.println(marks[3]);
+        // System.out.println(marks[4]);
+
+
+
+    }
+}
